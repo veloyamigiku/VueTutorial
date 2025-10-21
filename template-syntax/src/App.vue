0d560b52e8065ed107d7c53a2fcb5824a3929a80
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2025-10-21 14:28:58 +0900

    ・ガイド【テンプレート構文】を追加する。

---
+++ template-syntax/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import VBind from './components/VBind.vue';
+
+export default {
+  components: {
+    VBind
+  }
+}
+</script>
+
+<template>
+  <h1>テンプレート構文</h1>
+  <div>
+    <h2>属性バインディング</h2>
+    <VBind />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ template-syntax/src/components/VBind.vue
@@ -0,0 +1,27 @@
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const dynamicId1 = ref("id1")
+    const dynamicId2 = ref("id2")
+    const id = ref("id3")
+
+    return {
+      dynamicId1,
+      dynamicId2,
+      id
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <div v-bind:id="dynamicId1"></div>
+    <!-- v-bindの値がnullの場合は、属性がレンダリングされない -->
+    <div v-bind:id="null"></div>
+    <div :id="dynamicId2"></div>
+    <div :id></div>
+  </div>
+</template>
